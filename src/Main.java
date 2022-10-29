31f6f3a38bdaf01fd24e040414f18b06d3c0f98f
Тимофей <timon.from25@gmail.com>
2022-10-29 21:16:51 +0700

    Homework3 part 2

--- src/Main.java
+++ src/Main.java
@@ -63,8 +63,66 @@ public class Main {
                 System.out.println("Большее число " +two);}
                 else if (free>one&&free>two){
                 System.out.println("Большее число " +free);}
-            }
+
+                int clientOS = 1; //1 ios, 0 android
+                int clientDeviceYear = 2012;
+
+        if (clientOS==1&&clientDeviceYear>=2015) {
+            System.out.println( "Установите версию приложения для iOS по ссылке");}
+            else if (clientOS==1&&clientDeviceYear<2015){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+    else if (clientOS!=1&&clientDeviceYear>=2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");}
+    else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
+
+//Задание 3
+        int year = 2024;
+    if (year%4 == 0&&year%400 ==0||year % 100 != 0) {
+        System.out.println(year +" год является високосным");}
+    else {System.out.println(year + " год не является високосным");}
+
+ // Задание 4
+        int deliveryDistance = 60;
+    int deliveryDays = 1;
+    if (deliveryDistance>20){deliveryDays++;}
+    if (deliveryDistance>60){deliveryDays++;}
+        System.out.println("Доставка займёт " + deliveryDays + " дня");
+
+  //Задание 5
+int monthNumber = 12;
+switch (monthNumber){
+    case 1:
+    case 2:
+    case 12:
+        System.out.println("Сейчас Зима");
+    break;
+    case 3:
+    case 4:
+    case 5:
+         System.out.println("Сейчас Весна");
+    break;
+    case 6:
+    case 7:
+    case 8:
+        System.out.println("Сейчас Зима");
+    break;
+    case 9:
+    case 10:
+    case 11:
+        System.out.println("Сейчас Зима");
+    break;
+    default:
+        System.out.println("На венере всегда лето");
+}
+
+
+    }}
+
+
+
+
 
 
 
